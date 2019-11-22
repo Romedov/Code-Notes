7eb46d179878a66a20edae2bcef44000b3bd63b8
Romedov <55809111+Romedov@users.noreply.github.com>
2019-11-22 22:14:57 +0300

    Add files via upload

---
+++ C-Sharp/MVVM Example/Models/MainModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp2.Models
+{
+    public class MainModel : INotifyPropertyChanged
+    {
+        public MainModel()
+        {
+            Count1 = 0;
+            Count2 = 0;
+            Str = "Fock u!";
+        }
+        private int _c1;
+        private int _c2;
+        private string _str;
+        public int Count1
+        {
+            get { return _c1; }
+            set { _c1 = value; OnPropertyChanged(); }
+        }
+        public int Count2
+        {
+            get { return _c2; }
+            set { _c2 = value; OnPropertyChanged(); }
+        }
+        public string Str
+        {
+            get { return _str; }
+            set { _str = value; OnPropertyChanged(); }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ C-Sharp/MVVM Example/ViewModels/MainWindowVM.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp2.Models;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp2.ViewModels
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        public MainWindowVM()
+        {
+            mainModel = new MainModel();
+        }
+        public MainModel mainModel { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        public int Count1
+        {
+            get { return mainModel.Count1; }
+            set
+            {
+                mainModel.Count1 = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Count2
+        {
+            get { return mainModel.Count2; }
+            set
+            {
+                mainModel.Count2 = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Str
+        {
+            get { return mainModel.Str; }
+            set { mainModel.Str = value; OnPropertyChanged(); }
+        }
+        public ICommand Cmd1
+        {
+            get
+            {
+                return new DelegadeCommand((obj) =>
+                {
+                    Count1++;
+                });
+            }
+        }
+        public ICommand Cmd2
+        {
+            get
+            {
+                return new DelegadeCommand((obj) =>
+                {
+                    Count2++;
+                });
+            }
+        }
+        public ICommand Cmd3
+        {
+            get
+            {
+                return new DelegadeCommand((obj) =>
+                {
+                    PasswordBox passwordBox = obj as PasswordBox;
+                    MessageBox.Show(passwordBox.SecurePassword.ToString());
+                });
+            }
+        }
+    }
+}
