f579953fd71a3e104ce56ea9511bef501323d3c8
Romedov <55809111+Romedov@users.noreply.github.com>
2019-11-24 21:06:51 +0300

    Add files via upload

---
+++ C-Sharp/Cryptography/SHA256ManagedHasher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CSharpLearn
+{
+    public static class SHA256ManagedHasher
+    {
+        public static string GetHash(string str, Encoding encode)
+        {
+            byte[] data = encode.GetBytes(str);
+            var result = new SHA256Managed().ComputeHash(data);
+            return BitConverter.ToString(result).Replace("-", "").ToLower();
+        }
+        public static bool HashEquals(string inputStr, string encryptedStr, Encoding encode)
+        {
+            byte[] data = encode.GetBytes(inputStr);
+            var result = new SHA256Managed().ComputeHash(data);
+            return BitConverter.ToString(result).Replace("-", "").ToLower() == encryptedStr;
+        }
+    }
+}
