58cccc5cf6851935a3fc6b16fbff5d43de135f67
Romedov <55809111+Romedov@users.noreply.github.com>
2020-03-11 21:13:20 +0300

    Add files via upload

---
+++ C-Sharp/Xamarin/Orientation Switch (with MVVM)/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AdaptiveLayoutTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Views.MainPageView();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
