ed4e05862ea274dde1b29a16f144cc4050b22f94
Romedov <55809111+Romedov@users.noreply.github.com>
2020-03-11 21:17:44 +0300

    Add files via upload

---
+++ C-Sharp/Xamarin/Xamarin/Orientation Switch (with MVVM)/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdaptiveLayoutTest.ViewModels
+{
+    public class MainViewModel
+    {
+        public string PortraitLabel { get; set; } = "Portrait from VM";
+        public string LandscapeLabel { get; set; } = "Landscape from VM";
+    }
+}
---
+++ C-Sharp/Xamarin/Xamarin/Orientation Switch (with MVVM)/Views/MainPageView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AdaptiveLayoutTest.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPageView : ContentPage
+    {
+        private readonly View _portraitContent;
+        private readonly View _landscapeContent;
+        public MainPageView()
+        {
+            InitializeComponent();
+            this._portraitContent = new AdaptiveLayoutTest.Views.Portrait.MainViewPortrait();
+            this._landscapeContent = new AdaptiveLayoutTest.Views.Landscape.MainViewLandscape();
+            this.BindingContext = new ViewModels.MainViewModel();
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            if (width > height)
+            {
+                this.Content = _landscapeContent;
+            }
+            else
+            {
+                this.Content = _portraitContent;
+            }
+        }
+    }
+}
