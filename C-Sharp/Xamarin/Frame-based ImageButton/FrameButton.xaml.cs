16ccf5b79a9c1c48c61be9d08ad466706bf58a58
Romedov <55809111+Romedov@users.noreply.github.com>
2020-04-14 12:43:00 +0300

    Add files via upload

--- C-Sharp/Xamarin/Frame-based ImageButton/FrameButton.xaml.cs
+++ C-Sharp/Xamarin/Frame-based ImageButton/FrameButton.xaml.cs
@@ -2,11 +2,12 @@
 using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.ComponentModel;
 
 namespace GradProjMobile.Views.UserControls
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class FrameButton : Frame
+    public partial class FrameButton : Frame, INotifyPropertyChanged
     {
         #region Constructors
         public FrameButton()
@@ -14,6 +15,7 @@ namespace GradProjMobile.Views.UserControls
             InitializeComponent();
             BindingContext = this;
         }
+        
         #endregion
         #region Events
         public event EventHandler<EventArgs> Tapped;
@@ -22,7 +24,7 @@ namespace GradProjMobile.Views.UserControls
         #endregion
         #region Public props
         public static readonly BindableProperty TextProperty =
-                BindableProperty.Create("Text", typeof(string), 
+                BindableProperty.Create("Text", typeof(string),
                     typeof(FrameButton), string.Empty);
 
         public static readonly BindableProperty TextColorProperty =
@@ -52,6 +54,16 @@ namespace GradProjMobile.Views.UserControls
         public static readonly BindableProperty ImageSourceProperty =
                 BindableProperty.Create("ImageSource", typeof(ImageSource),
                     typeof(FrameButton), null);
+
+        public static readonly BindableProperty DirectionProperty =
+                BindableProperty.Create("Direction", typeof(FlexDirection),
+                    typeof(FrameButton), FlexDirection.Row,
+                    propertyChanged: (bindable, oldVal, newVal) =>
+                    {
+                        var direction = (FlexDirection)newVal;
+                        var instance = bindable as FrameButton;
+                        OnDirectionChanged(instance, direction);
+                    });
         public string Text
         {
             get { return (string)GetValue(TextProperty); }
@@ -92,6 +104,11 @@ namespace GradProjMobile.Views.UserControls
             get { return (ImageSource)GetValue(ImageSourceProperty); }
             set { SetValue(ImageSourceProperty, value); }
         }
+        public FlexDirection Direction
+        {
+            get { return (FlexDirection)GetValue(DirectionProperty); }
+            set { SetValue(DirectionProperty, value); }
+        }
         #endregion
         #region Public methods
         #endregion
@@ -115,8 +132,33 @@ namespace GradProjMobile.Views.UserControls
                 Command?.Execute(resolvedParameter);
             }
         }
+
         #endregion
         #region Private methods
+        private static void OnDirectionChanged(FrameButton instance, FlexDirection direction)
+        {
+            switch (direction)
+            {
+                case FlexDirection.Row:
+                    AlignContent(instance, new Thickness(0, 0, 10, 0), LayoutOptions.StartAndExpand, TextAlignment.Start);
+                    break;
+                case FlexDirection.RowReverse:
+                    AlignContent(instance, new Thickness(10, 0, 0, 0), LayoutOptions.StartAndExpand, TextAlignment.Start);
+                    break;
+                case FlexDirection.Column:
+                    AlignContent(instance, new Thickness(0, 0, 0, 10), LayoutOptions.CenterAndExpand, TextAlignment.Center);
+                    break;
+                case FlexDirection.ColumnReverse:
+                    AlignContent(instance, new Thickness(0, 10, 0, 0), LayoutOptions.CenterAndExpand, TextAlignment.Center);
+                    break;
+            }
+        }
+        private static void AlignContent(FrameButton instance, Thickness margin, LayoutOptions layout, TextAlignment text)
+        {
+            instance.ButtonImage.Margin = margin;
+            instance.ButtonText.HorizontalOptions = layout;
+            instance.ButtonText.HorizontalTextAlignment = text;
+        }
         #endregion
     }
 }
