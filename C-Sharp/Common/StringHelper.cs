ae04e6052366bdba08b900ac49fe1658dc889b03
Romedov <55809111+Romedov@users.noreply.github.com>
2020-05-07 13:02:17 +0300

    Add files via upload

---
+++ C-Sharp/Common/StringHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+namespace RiderConsoleTest
+{
+    public static class StringHelper
+    {
+        public static string TruncateCommas(string str, char comma)
+        {
+            if (str.Count(ch=>ch==',') > 1)
+            {
+                str = Reverse(Reverse(str).Remove(0, Reverse(str).IndexOf(comma) + 1));
+                str = TruncateCommas(str, comma);
+            }
+            return str;
+        }
+        
+        public static string Reverse(string str) => new string(str.ToCharArray().Reverse().ToArray());
+    }
+}
